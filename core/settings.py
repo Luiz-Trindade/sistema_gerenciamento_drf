3c78dde0351f426dc7cd1ff25b29ee0e1890bbad
Luiz-Trindade <luiz.gabriel.m.trindade@gmail.com>
2026-09-19 12:54:12 -0300

    feat: add import_export

--- core/settings.py
+++ core/settings.py
@@ -158,6 +158,9 @@ SPECTACULAR_SETTINGS = {
 }
 
 UNFOLD = {
+    "SITE_TITLE": "Simples Gestão",
+    "SITE_HEADER": "Simples Gestão",
+    "SITE_SUBHEADER": "Administração",
     "COLORS": {
         "primary": {
             "50": "#eff6ff",
@@ -171,6 +174,23 @@ UNFOLD = {
             "800": "#1e40af",
             "900": "#1e3a8a",
             "950": "#172554",
+        },
+    },
+    "THEMES": {
+        "dark": {
+            "primary": {
+                "50": "#ecfdf5",
+                "100": "#d1fae5",
+                "200": "#a7f3d0",
+                "300": "#6ee7b7",
+                "400": "#34d399",
+                "500": "#10b981",
+                "600": "#059669",
+                "700": "#047857",
+                "800": "#065f46",
+                "900": "#064e3b",
+                "950": "#022c22",
+            },
         },
     },
     "BORDER_RADIUS": "1.5px",
