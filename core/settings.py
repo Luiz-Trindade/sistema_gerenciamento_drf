a3d74d1d338cd02404b63b4621429cd8d04f4572
Luiz-Trindade <luiz.gabriel.m.trindade@gmail.com>
2026-09-20 15:57:45 -0300

    feat: add ACCESS_TOKEN_LIFETIME

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-auq$a_itrtcohki5ilzxk5zpqx*fboirc2sehbx0i(*_ri_2$g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
